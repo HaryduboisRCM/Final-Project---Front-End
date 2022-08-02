2ca3f32503a96e2d333b387d884f723085531c8a
Tom S <harydubois101@protonmail.com>
2022-08-02 13:56:30 +0100

    Updates

---
+++ precache-manifest.ea07ef1466f68af63590bf6883615a51.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7ea4c57f12d3394d581cf2975e21df2",
+    "url": "/Final-Project---Front-End/index.html"
+  },
+  {
+    "revision": "28294ab910e7b17c73de",
+    "url": "/Final-Project---Front-End/static/css/2.e5cecc0c.chunk.css"
+  },
+  {
+    "revision": "f82a78ac48c313238006",
+    "url": "/Final-Project---Front-End/static/css/main.09b51a81.chunk.css"
+  },
+  {
+    "revision": "28294ab910e7b17c73de",
+    "url": "/Final-Project---Front-End/static/js/2.3e71b667.chunk.js"
+  },
+  {
+    "revision": "e02d1411ade13ad5b70306a37e918baa",
+    "url": "/Final-Project---Front-End/static/js/2.3e71b667.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f82a78ac48c313238006",
+    "url": "/Final-Project---Front-End/static/js/main.37ec1fd1.chunk.js"
+  },
+  {
+    "revision": "eff174e47cef75f9893c",
+    "url": "/Final-Project---Front-End/static/js/runtime-main.7471dbd3.js"
+  },
+  {
+    "revision": "f42257c1571d87a4756d2240867407e1",
+    "url": "/Final-Project---Front-End/static/media/LinkedInLogo.f42257c1.png"
+  },
+  {
+    "revision": "ce374ec48f373cb067f6a6cec0929cfd",
+    "url": "/Final-Project---Front-End/static/media/Portfolio.ce374ec4.png"
+  },
+  {
+    "revision": "6d7378258909ec4c59d501b2a7becca2",
+    "url": "/Final-Project---Front-End/static/media/TDA Logo.6d737825.jpg"
+  }
+]);
